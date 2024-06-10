1230f278b4b5b91e60d185c2de0e203314839af5
Trevor <trevors19themail@gmail.com>
2024-06-10 15:05:15 -0600

    somthing happend

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine ("Bababooie.");
+        for (int j =0; j < 10; j++ )
+            Console.WriteLine (j);
     }
 }
