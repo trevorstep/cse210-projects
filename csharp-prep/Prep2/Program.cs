0392a955267b51be47d61dee07bbd90a955bbdfd
Trevor <trevors19themail@gmail.com>
2024-06-11 17:40:15 -0600

    Added If-else if - else statements and made it so the variable letters provided the user witht there letter grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello my dude, what is your letter grade percentage?");
+        int letterGrade = int.Parse( Console.ReadLine());    
+
+
+        string letter = "";
+
+    if (letterGrade >= 90)
+        {
+        letter = "A";
+        }
+        
+    else if (letterGrade >= 80)
+        {
+        letter = "B";
+        }
+
+    else if (letterGrade >= 70)
+        {
+        letter = "C";
+        }
+    else if (letterGrade >= 60)
+        {
+        letter = "D";
+        }
+    else 
+        {
+        letter = "F";
+        }
+    
+    if (letterGrade>= 70)
+    {
+        Console.WriteLine("You passed"!);
+    }
+    else
+    {
+        Console.WriteLine("You did not pass");
+    }
+
+        Console.WriteLine ($"Your grade is an {letter}.");
+
     }
 }
