2754c0f5a3c195bed593399c190f5fd14cebc507
Trevor <trevors19themail@gmail.com>
2024-06-11 13:59:08 -0600

    console  write and read lines, bababooie

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Whats your first name shawtyh?");
+        string First_name = Console.ReadLine ();
+
+        Console.WriteLine("Whats your last name shawtyh?");
+        string Last_name = Console.ReadLine ();
+
+        Console.WriteLine("");
+        Console.WriteLine ($"Your name is {Last_name}, {First_name} {Last_name}.");
+
     }
 }
