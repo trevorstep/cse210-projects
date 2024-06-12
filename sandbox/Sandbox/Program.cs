a6d6a4bf4bc4952ffb07536f6b87c1d0fc07c64b
Trevor <trevors19themail@gmail.com>
2024-06-12 14:35:07 -0600

    Added while, do and for loop

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,9 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-        Console.WriteLine ("Bababooie.");
-        for (int j =0; j < 10; j++ )
-            Console.WriteLine (j);
+        // Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine ("Bababooie.");
+        // for (int j =0; j < 10; j++ )
+        //     Console.WriteLine (j);
+
+
+        // int UserAge = -1;
+
+        // while (UserAge < 0 || UserAge > 120)
+        // {
+        //     Console.WriteLine("Please input your age (0-120): ");
+        //     UserAge = int.Parse (Console.ReadLine());
+        // }
+
+        // int userAge2 =65561561;
+        // do
+        // {
+        //     Console.WriteLine ("Please input your age (0-120):");
+        //     userAge2 = int.Parse (Console.ReadLine());
+        // } while (userAge2 <0 || userAge2 > 120); 
+
+
+        for (int i = 0; i<100; i++) //first set creates varible, second will continue to run the for loop as long as it remains true, third will add 1 at the end of every itteration. 
+        Console.WriteLine(""+i);
+
+    
+    
     }
 }
