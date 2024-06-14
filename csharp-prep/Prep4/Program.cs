6f4ab8a4ff52a931d01fb106ae26de8ee7bd412b
Trevor <trevors19themail@gmail.com>
2024-06-14 15:56:31 -0600

    added a biggest number counter and smallest number counter

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -10,15 +10,22 @@ class Program
         List<int> listValues = new List<int>();
         int userInput = 0;
         int sumTotal = 0;
+        int biggest = 0;
+        int smallest = 999999999;
         int averageCounter = 0;
 
         do
         {
-            Console.WriteLine("Enter a list of numbers, type 0 when you are satisfied with the amount of 0's");
+            Console.WriteLine("Enter a list of numbers, type 0 when you are satisfied with the amount of number's");
             string userInput2 = Console.ReadLine();
             userInput = int.Parse(userInput2);
             listValues.Add(userInput);
             averageCounter++;
+            if (userInput > biggest)
+                biggest = userInput;
+            if (userInput < smallest)
+                smallest = userInput;
+
         } while (userInput != 0);
 
         foreach (int number in listValues)
@@ -27,9 +34,12 @@ class Program
         }
         Console.WriteLine($"The total sum is {sumTotal}");
 
-        int totalAverage =  sumTotal/ averageCounter;
+        int totalAverage = sumTotal / averageCounter;
 
-        Console.WriteLine ($"The average is {totalAverage}");
+        Console.WriteLine($"The average is {totalAverage}");
+                Console.WriteLine($"The biggest number is {biggest}");
+        Console.WriteLine($"The smallest is {smallest}");
+        
 
 
     }
