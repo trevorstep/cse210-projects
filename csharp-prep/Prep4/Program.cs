341e16ed27c27cc9b1e0aee38c61590cd8927074
Trevor <trevors19themail@gmail.com>
2024-06-14 15:06:45 -0600

    Made a program that gets a list of numbers from the user then computes the average.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> listValues = new List<int>();
+        int userInput = 0;
+        int sumTotal = 0;
+        int averageCounter = 0;
+
+        do
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when you are satisfied with the amount of 0's");
+            string userInput2 = Console.ReadLine();
+            userInput = int.Parse(userInput2);
+            listValues.Add(userInput);
+            averageCounter++;
+        } while (userInput != 0);
+
+        foreach (int number in listValues)
+        {
+            sumTotal += number;
+        }
+        Console.WriteLine($"The total sum is {sumTotal}");
+
+        int totalAverage =  sumTotal/ averageCounter;
+
+        Console.WriteLine ($"The average is {totalAverage}");
+
+
     }
 }
