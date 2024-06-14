9559afc0d3d0057fe2aa698f1979811634c07871
Trevor <trevors19themail@gmail.com>
2024-06-14 14:20:11 -0600

    Strech challenge, Added counter

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        int response;
+        Console.WriteLine(number);
+        int UserGuess = 0;
+
+    do
+        {
+            Console.WriteLine("What is your magic number?");
+            string responseAsStrng = Console.ReadLine();
+            UserGuess = UserGuess + 1;
+            response = int.Parse(responseAsStrng);
+
+
+            if (response == number)
+            {
+                Console.WriteLine("You got it!");
+                Console.WriteLine($"You guessed{UserGuess}");
+            }
+            else if (response < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (response > number)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (response != number);
+
     }
+
 }
