15a13397abf55169ff8c3b9b74b943a9086a8dcc
Trevor <trevors19themail@gmail.com>
2024-06-20 14:41:43 -0600

    I dont even remember what I changed but im sure its important

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
