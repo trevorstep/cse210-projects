3b847bebfcf2789630ac485ab0177c35316f7ca8
Trevor <trevors19themail@gmail.com>
2024-06-23 20:00:18 -0600

    Im getting a weird error but I think it actaully might be the compiler/ environment. I'll play with this more later, addtionally I realized that I wrote the same method twice both in this class and in the Journal class so I deleted this one seeing as it was causing problems.

--- prove/Develop02/JournalEntryClass.cs
+++ prove/Develop02/JournalEntryClass.cs
@@ -4,35 +4,15 @@ class JournalEntry
     public string _prompt;
     public string _response;
 
-    public string filename = "Journal.txt";
-
     public JournalEntry(string date, string prompt, string response)
     {
         _date = date;
         _prompt = prompt;
         _response = response;
-
     }
 
     public override string ToString()
     {
-        string outputString = "";
-        outputString = $"{_date}#{_prompt}#{_response}";
-        return outputString;
+        return $"{_date}#{_prompt}#{_response}";
     }
-
-    public void ReadFromFile(string filename)
-    {
-        string[] lines = System.IO.File.ReadAllLines(filename);
-        foreach (string line in lines)
-        {
-            string[] parts = line.Split("#");
-            string date = parts[0];
-            string question = parts[1];
-            string entryText = parts[2];
-            JournalEntry entry = new JournalEntry(date, question, entryText);
-            this.AddEntry(entry);
-        }
-    }
-
 }
