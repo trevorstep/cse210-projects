2a2462f9e8ca6ce93b192a20416a6a37c38bd48b
Trevor <trevors19themail@gmail.com>
2024-06-23 20:37:47 -0600

    Accdientally had  Load and Save in the wrong order, I need to figure out why my Load isnt putting spaces inbetween where the hashtags are, but besides that the program is operational

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -14,8 +14,8 @@ class Program
             Console.WriteLine("Please select one of the following choices.");
             Console.WriteLine("1. Write");
             Console.WriteLine("2. Display");
-            Console.WriteLine("3. Load");
-            Console.WriteLine("4. Save");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
             Console.WriteLine ("5. Quit");
 
             string responseAsStrng = Console.ReadLine();
