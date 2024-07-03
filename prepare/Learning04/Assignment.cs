6dc7550a649edfb159d0298f7c78883ca95fdf21
Trevor <trevors19themail@gmail.com>
2024-07-02 22:13:24 -0600

    Comments on what each method and constructor does.

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -1,19 +1,37 @@
+using System.Dynamic;
+
 class Assignment
 {
     private string _studentName = "";
     private string _topic = "";
 
+// Basic constructor.
 public Assignment (string studentName, string topic)
 {
     _studentName = studentName;
     _topic = topic;
 }
 
-
+// Gets student names and topic ( accesses them.)
+#region 
 public string GetStudentName()
 {
 
     return _studentName;
 }
+
+public string GetTopic()
+{
+    return _topic;
+}
+#endregion
+
+// This Method concatnates the student name and the topic togther, very handy for all parent and child functions
+public string GetSummary()
+{
+    return _studentName +"-"+_topic;
+}
+
+
 
 }
