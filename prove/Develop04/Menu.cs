be77a79d67c30e5008eea890c3858346814b0080
Trevor <trevors19themail@gmail.com>
2024-07-03 14:57:01 -0600

    Implimented Menu class.

---
+++ prove/Develop04/Menu.cs
@@ -0,0 +1,29 @@
+class Menu
+{
+    public int DisplayMenu()
+    {
+        int response = 0;
+
+        while (response < 1 || response > 4)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu options");
+            Console.WriteLine("1. Start Breathing activity");
+            Console.WriteLine("2. Start Reflectiong Activity");
+            Console.WriteLine("3. Start Listening activity");
+            Console.WriteLine("4. Quit");
+
+            try
+            {
+                response = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("That is an invalid response, please enter a number between 1 and 5.");
+            }
+
+        }
+        return response;
+
+    }
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int response = 0;
+        while (response != 4)
+        {
+            Menu menu = new Menu();
+            response = menu.DisplayMenu();
+            if (response == 1)
+            {
+                // Call breathing actvity
+
+            }
+            else if (response == 2)
+            {
+
+            }
+            else if (response == 3)
+            {
+
+            }
+        }
     }
 }
