a12ee4764bf7da797c68b0430fc164c9198674cf
Trevor <trevors19themail@gmail.com>
2024-07-04 12:59:01 -0600

    Createed the DisplaySpinner method, it still needs an exit condtion, addtionally added GetDescription Method, However this method for some reason wont call the _description attribute so I need to figure out why thats being wonky

--- prove/Develop04/Actvity.cs
+++ prove/Develop04/Actvity.cs
@@ -1,27 +1,52 @@
+using System.ComponentModel.DataAnnotations;
+
 class Activity
 {
- private int _time= 0;
- private string typeOfActvity = "";
- private string _description ="";
- private DateTime _endTime;
- 
-public void DisplayWelcomeMessage (string typeOfActvity)
-{
-    Console.WriteLine ($"Welcome to the {typeOfActvity} Actvity!");
-} 
+    private int _time = 0;
+    private string typeOfActvity = "";
+    private string _description = "";
+    private bool _exitTime = true;
+    private DateTime _endTime;
 
-public void DisplayExitMessage (string typeOfActvity)
-{
-    Console.WriteLine ($"Thank you for doing the {typeOfActvity} Actvity!");
-}
+    public void DisplayWelcomeMessage(string typeOfActvity)
+    {
+        Console.WriteLine($"Welcome to the {typeOfActvity} Actvity!");
+    }
 
-public void GetDuration()
-{
-    Console.WriteLine ("How long in seconds, would you like for your session to take?");
-    _time  = int.Parse(Console.ReadLine());
-}
+    public void GetDescription(string _description)
+    {
+        Console.WriteLine($"{_description}");
+    }
+
+    public void DisplayExitMessage(string typeOfActvity)
+    {
+        Console.WriteLine($"Thank you for doing the {typeOfActvity} Actvity!");
+    }
+
+    public void GetDuration()
+    {
+        Console.WriteLine("How long in seconds, would you like for your session to take?");
+        _time = int.Parse(Console.ReadLine());
+    }
+
+    public void Displayspinner()
+    {
+        string[] pattern = { "_", "_", "_", "-", "`", "`", "'", "´", "-", "_", "_", "_" };
+
+        while (_exitTime == true)
+        {
+            foreach (var symbol in pattern)
+            {
+                Console.Write(symbol);
+                Thread.Sleep(45);
+                Console.Write("\b \b");
 
 
+            }
+        }
+
+
+    }
 
 
 
