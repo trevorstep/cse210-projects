b703e94100e2e81f5ff7e237cb223f810bfc205c
Trevor <trevors19themail@gmail.com>
2024-07-11 13:54:02 -0600

    Started Menu class

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
-    }
-}
+        int response = 0;
+        while (response != 4)
+        {
+            Menu menu = new Menu();
+            response = menu.DisplayMenu();
+            if (response == 1)
+            {
