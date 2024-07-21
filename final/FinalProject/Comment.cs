db412a561555f21fd25b23d135efadf07122f45a
Trevor <trevors19themail@gmail.com>
2024-07-21 16:27:31 -0600

    added Getters and setters to acces private variables, and then had ChatGPT help make me some youtube cideos and add them to my list, as well as the comments.s.

--- final/FinalProject/Comment.cs
+++ final/FinalProject/Comment.cs
@@ -1,3 +1,20 @@
 class Comment{
-    
+    private string _userName;
+    private string _text;
+
+    public Comment (string userName, string text)
+    {
+        _userName = userName;
+        _text = text;
+    }
+
+        public string UserName
+    {
+        get { return _userName; }
+    }
+
+        public string Text
+    {
+        get { return _text; }
+    }
 }
--- final/FinalProject/Video.cs
+++ final/FinalProject/Video.cs
@@ -1,15 +1,45 @@
-class Video{
-
-private string _title;
-private string _author;
-private int _videoLengthInSeconds;
-private static List <Comment> comments = new List<Comment>();
-
-public Video (string videos)
+class Video
 {
+    private string _title;
+    private string _author;
+    private int _videoLengthInSeconds;
+    private List<Comment> _comments;
 
-}
+    public Video(string title, string author, int videoLengthInSeconds)
+    {
+        _title = title;
+        _author = author;
+        _videoLengthInSeconds = videoLengthInSeconds;
+        _comments = new List<Comment>();
+    }
+    public string Title
+    {
+        get { return _title; }
+    }
+    public string Author
+    {
+        get { return _author; }
+    }
 
+    public int VideoLengthInSeconds
+    {
+        get { return _videoLengthInSeconds; }
+    }
 
+#region 
+// Chat GPT helped me with these since I couldnt figure out how to access my data types.
+    public void AddComment(Comment comment){
+        _comments.Add(comment);
+    }
+    public List<Comment> GetComments(){
+        return new List<Comment>(_comments);
+    }
+#endregion
+
+    public int GetNumberOfComments()
+    {
+        return _comments.Count;
+
+    }
 
 }
